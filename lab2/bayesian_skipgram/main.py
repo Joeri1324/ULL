cdd4173b448b9c964572129d0db9e784afc8f92d
joeri1324 <joeri.sleegers@student.uva.nl>
2018-05-02 18:01:39 +0200

    seems to work

--- lab2/bayesian_skipgram/main.py
+++ lab2/bayesian_skipgram/main.py
@@ -1,6 +1,10 @@
 import torch
 from read import read_context_wise, read_vocab
 from model import BayesianSkipgram
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.optim as optim
+import torch
 
 FILE = '../data/dev.en'
 
@@ -8,11 +12,22 @@ vocab = {w: i for i, w in enumerate(read_vocab(FILE, []))}
 inv_vocab = {i: w for i, w in enumerate(read_vocab(FILE, []))}
 
 model = BayesianSkipgram(10, len(vocab), 4)
+loss_function = nn.NLLLoss()
+optimizer = optim.Adam(model.parameters(), lr = 0.001)
 
-for word, context in read_context_wise(FILE, 2, []):
-    word_index = torch.LongTensor([vocab[word]])
+def train():
+    losses = []
+    for word, context in read_context_wise(FILE, 2, []):
+        word_index = torch.LongTensor([vocab[word]])
+        optimizer.zero_grad()
+        model.zero_grad()
 
-    context_indices = torch.LongTensor([vocab[i] for i in context])
-    model(word_index, context_indices)
+        context_indices = torch.LongTensor([vocab[i] for i in context])
+        loss = model(word_index, context_indices)
+        losses.append(loss)
+        loss.backward()
+        optimizer.step()
+    print('loss', torch.mean(torch.Tensor(losses)))
 
-    break
+for _ in range(10):
+    train()
--- lab2/bayesian_skipgram/model.py
+++ lab2/bayesian_skipgram/model.py
@@ -4,22 +4,27 @@ import torch.nn.functional as F
 import torch.distributions as dist
 
 
+NEGATIVE_SAMPLE_SIZE = 10
+
 class BayesianSkipgram(nn.Module):
     
     def __init__(self, embedding_size, vocab_size, context_size):
         super(BayesianSkipgram, self).__init__()
 
         # Have to check the dimension embedding size
-        self.Embedding =  nn.Embedding(vocab_size, embedding_size, sparse=True)
+        self.Embedding =  nn.Embedding(vocab_size, embedding_size)
         self.M = nn.Linear(embedding_size, context_size)
         self.U = nn.Linear(context_size*2, context_size)
         self.W = nn.Linear(context_size*2, context_size)
 
         # have to think of proper initialization
-        self.prior_mus = nn.Embedding(vocab_size, context_size, sparse=True)
-        self.prior_sigmas = nn.Embedding(vocab_size, context_size, sparse=True)
+        self.prior_mus = nn.Embedding(vocab_size, context_size)
+        self.prior_sigmas = nn.Embedding(vocab_size, context_size)
+
+        self.negative_sampler = torch.distributions.uniform.Uniform(torch.tensor([0]), torch.tensor([vocab_size]))
 
     def forward(self, x, context):
+
         Rw = self.M(self.Embedding(x).repeat(len(context), x.size()[0]))
         Rc = self.M(self.Embedding(context))
 
@@ -29,12 +34,30 @@ class BayesianSkipgram(nn.Module):
 
         prior = dist.multivariate_normal.MultivariateNormal(
             self.prior_mus(x),
-            covariance_matrix=torch.diagflat(self.prior_sigmas(x))
+            covariance_matrix=torch.diagflat(torch.mul(self.prior_sigmas(x), self.prior_sigmas(x)))
         )
         posterior = dist.multivariate_normal.MultivariateNormal(
             mu,
             covariance_matrix=torch.diagflat(sigma)
         )
+        vocab_size = self.Embedding.weight.size()[0]
 
-        divergence = dist.kl.kl_divergence(prior, posterior)
-        print('divv', divergence)
+        likelihood = 0
+        for j in context:
+            positive = dist.multivariate_normal.MultivariateNormal(
+                self.prior_mus(j),
+                covariance_matrix=torch.diagflat(torch.mul(self.prior_sigmas(j), self.prior_sigmas(j)))
+            )
+            negative_samples = torch.LongTensor([int(i) for i in 
+                torch.Tensor(NEGATIVE_SAMPLE_SIZE).random_(to=vocab_size)
+            ])
+            for k in negative_samples:
+
+                negative = dist.multivariate_normal.MultivariateNormal(
+                    self.prior_mus(k),
+                    covariance_matrix=torch.diagflat(torch.mul(self.prior_sigmas(k), self.prior_sigmas(k)))
+                )
+                likelihood += max(0, dist.kl.kl_divergence(posterior, negative)
+                                  - dist.kl.kl_divergence(posterior, positive) + 1) 
+                                  # 1 should b m for batches
+        return likelihood - dist.kl.kl_divergence(posterior, prior)
