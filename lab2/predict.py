08f8b7361d4c5e99d59949eec6e2a185560d0d87
Poezedoez <raggerjonkers@gmail.com>
2018-05-17 14:54:28 +0200

    should predict and write correctly now

--- lab2/predict.py
+++ lab2/predict.py
@@ -4,15 +4,15 @@ import numpy as np
 
 def write_predictions(testfile, model, vocab):
     with open(testfile) as infile:
-        with open('predictions.txt', 'w') as outfile:
-            predictions = csv.writer(outfile, delimiter='\t')
+        with open('lst/predictions.txt', 'w') as outfile:
+            predictions = csv.writer(outfile, delimiter='\t', quotechar='|', quoting=csv.QUOTE_NONE, escapechar=":")
             test = csv.reader(infile, delimiter='\t')
             for target, sentence_id, position, sentence in test:
                 # context = get_context(sentence, int(position))
-                candidates, scores = model.predict(target)
-                line = ['\t' + target + ' ' + str(sentence_id)]
-                for i in candidates:
-                    line.append(vocab[i] + ' '  + str(scores[i]))
+                candidates, scores = model.predict(target.split('.')[0])
+                line = ['RANKED' + '\t' + target + ' ' + str(sentence_id)]
+                for i, c in enumerate(candidates):
+                    line.append(vocab[c] + ' '  + str(scores[i]))
                 predictions.writerow(line)
                 
            
@@ -28,6 +28,7 @@ def get_context(sentence, position, window=4):
 
 
 if __name__ == '__main__':
-    # test_scores = [0.7, 0.65, 0.62, 0.9]
-    # test_words = ['table', 'chair', 'bike', 'kitchen']
-    write_predictions('lst/lst_test.preprocessed', None)
+    # vocab = {1:'kitchen', 2:'chair', 3:'bike', 4:'table'}
+    # candidates = [3, 2, 1, 4]
+    # scores = [0.8, 0.6, 0.4, 0.33]
+    write_predictions('lst/lst_test.preprocessed', skipgram, vocab)
